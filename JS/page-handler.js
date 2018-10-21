ac19842a69b0916449618d53c3fc9007d9e2f753
Joshua Styles <styles@outlook.com>
2018-10-21 22:18:37 +0100

    Structure
    Site file structure in place.
    Site design stucture prepared.
    Projects carousel added. Info panel added to side of carousel.

---
+++ JS/page-handler.js
@@ -0,0 +1,119 @@
+$(document).ready(function(){
+    
+    
+    
+    $("#home-btn").click(function(){
+        
+        $("#home-page").show();
+        $("#about-page").hide();
+        $("#projects-page").hide();
+        $("#contact-page").hide();
+        
+        resetAllText();
+        resetAllButtons();
+        addBTNSelected($("#home-btn"));
+        addTextSelected($("#home-text"));
+    })
+    
+    $("#about-btn").click(function(){
+        
+        $("#about-page").show();
+        $("#home-page").hide();
+        $("#projects-page").hide();
+        $("#contact-page").hide();
+        
+        resetAllText();
+        resetAllButtons();
+        addBTNSelected($("#about-btn"));
+        addTextSelected($("#about-text"));
+    })
+    
+    $("#projects-btn").click(function(){
+        
+        $("#projects-page").show();
+        $("#home-page").hide();
+        $("#about-page").hide();
+        $("#contact-page").hide();
+        
+        resetAllText();
+        resetAllButtons();
+        addBTNSelected($("#projects-btn"));
+        addTextSelected($("#projects-text"));
+        
+    })
+    
+    $("#contact-btn").click(function(){
+        
+        $("#contact-page").show();
+        $("#home-page").hide();
+        $("#about-page").hide();
+        $("#projects-page").hide();
+        
+        resetAllText();
+        resetAllButtons();
+        addBTNSelected($("#contact-btn"));
+        addTextSelected($("#contact-text"));
+    })
+    
+    $("#info-btn").click(function(){
+        
+        $("#info-result").show();
+        $("#tech-result").hide();
+        
+        $("#project-text-2").removeClass("selected");
+        $("#project-text-1").removeClass("unselected");
+        
+        $("#project-text-2").addClass("unselected");
+        $("#project-text-1").addClass("selected");
+    })
+    
+    $("#tech-btn").click(function(){
+        
+        $("#tech-result").show();
+        $("#info-result").hide();
+        
+        $("#project-text-1").removeClass("selected");
+        $("#project-text-2").removeClass("unselected");
+        
+        $("#project-text-1").addClass("unselected");
+        $("#project-text-2").addClass("selected");
+    })
+    
+    function resetAllText(){
+        
+        $("#home-text").removeClass("selected");
+        $("#about-text").removeClass("selected");
+        $("#projects-text").removeClass("selected");
+        $("#contact-text").removeClass("selected");
+        
+        $("#home-text").addClass("unselected");
+        $("#about-text").addClass("unselected");
+        $("#projects-text").addClass("unselected");
+        $("#contact-text").addClass("unselected");
+    }
+    
+    function resetAllButtons(){
+        
+        $("#home-btn").removeClass("dark");
+        $("#about-btn").removeClass("dark");
+        $("#projects-btn").removeClass("dark");
+        $("#contact-btn").removeClass("dark");
+        
+        $("#home-btn").addClass("darkest");
+        $("#about-btn").addClass("darkest");
+        $("#projects-btn").addClass("darkest");
+        $("#contact-btn").addClass("darkest");
+    }
+    
+    function addBTNSelected(element){
+        
+        element.removeClass("darkest");
+        element.addClass("dark");
+    }
+    
+    function addTextSelected(element){
+        
+        element.removeClass("unselected");
+        element.addClass("selected");
+    }
+});
